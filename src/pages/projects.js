f98ab6db86719d95f9f39d4ad47779f72ce02241
Johnathanblackburncodes <jhblackburn90@gmail.com>
2021-05-07 14:34:51 -0700

    inserted project query

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,7 +5,7 @@ import Projects from "../components/Projects"
 // ...GatsbyImageSharpFluid
 
 const ProjectsPage = () => {
-  return <h2>projects page</h2>
+  return <h2>projects page help</h2>
 }
 
 export default ProjectsPage
